4b39d6e2e995f82b208468d3516123bf90d4972c
Sonu Kumar <sonu82256@gmail.com>
2024-09-12 14:05:56 +0530

    project responsive

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -65,7 +65,7 @@ export default function page() {
                                     <button className="p-[3px] relative w-full">
                                         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
                                         <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                                            View Live App
+                                            View Live
                                         </div>
                                     </button>
                                 </div>
@@ -160,7 +160,7 @@ export default function page() {
                                     <button className="p-[3px] relative w-full">
                                         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
                                         <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent">
-                                            View Live App
+                                            View Live 
                                         </div>
                                     </button>
                                 </div>
